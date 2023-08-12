314110aa026e99164dbbadc3615be7ba70107a6f
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2023-08-12 19:58:38 +0300

    update: signUp, logIn

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,5 +1,6 @@
 package services;
 
+import enums.Role;
 import interfaces.UserStorage;
 import models.User;
 import storages.JsonFileUserStorage;
@@ -11,8 +12,15 @@ public class UserService {
 
     public User create(String username, String password) {
         User user = new User(username, password);
+        if (user.getUsername().equals("admin") && user.getPassword().equals("admin")) {
+            user.setRole(Role.ADMIN);
+            storage.add(user);
+        } else {
+            user.setRole(Role.USER);
+            storage.add(user);
+        }
 
-        return storage.add(user);
+        return user;
     }
 
     public User getById(UUID id) {
